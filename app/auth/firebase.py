c3faae01464f507e30da5a20807fd115c5b102de
orewa-arun <arunworkacc0@gmail.com>
2025-06-07 11:35:21 +0530

    modify:firebase - firebase credentials storage moved to .env file

--- app/auth/firebase.py
+++ app/auth/firebase.py
@@ -1,12 +1,20 @@
 import firebase_admin
 from firebase_admin import credentials
 import os
+import json
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
-
 if not firebase_admin._apps:
-    cred = credentials.Certificate(os.getenv("FIREBASE_CREDENTIALS_PATH"))
+    firebase_json = os.getenv("FIREBASE_CREDENTIALS")
+
+    if not firebase_json:
+        raise ValueError("FIREBASE_CREDENTIALS environment variable is missing.")
+
+    # Parse the JSON string into a dictionary
+    firebase_dict = json.loads(firebase_json)
+
+    # Use the dictionary to create credentials
+    cred = credentials.Certificate(firebase_dict)
     firebase_admin.initialize_app(cred)
